51e034663c83f4153142fc8d2fcfe30f451c5a36
Edmond choi <e2choi@ucsd.edu>
2020-05-13 06:10:42 +0000

    please pass the tests im running out of time

--- BWT.cpp
+++ BWT.cpp
@@ -1,10 +1,35 @@
 #include <string>
 #include "BWT.h"
+#include <algorithm>    
+#include <iostream>
+#include <vector>
 using namespace std;
 
 /**
  * Implement bwt() correctly
  */
+string bwtRotate(const string & s, int n ){
+    string rotate;
+    string  shiftedChar = s.substr(0,n);
+    string  restOfString = s.substr(n);
+    rotate+=restOfString;
+    rotate+=shiftedChar;
+    return rotate;
+}
+
 string bwt(const string & s) {
-    /* YOUR CODE HERE */
+    vector<string> rotatedStrings;
+    string rbwt;
+    for(unsigned int i = 0; i < s.length(); i++){
+        string rotated = bwtRotate(s,i);
+        rotatedStrings.push_back(rotated);
+    }
+    
+    sort(rotatedStrings.begin(), rotatedStrings.end());
+    // print out last character in each element of array
+    for( vector<string>::iterator iter = rotatedStrings.begin(); iter != rotatedStrings.end(); iter++){
+        string s = *iter;
+        rbwt += s.back();
+    }
+    return rbwt;
 }
